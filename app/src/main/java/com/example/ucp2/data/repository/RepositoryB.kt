602490d87df8b7f40baaf1ffed5207591b6c184d
safirasn21 <safirasn021@gmai.com>
2024-12-18 21:04:33 +0700

    menambahkan interface RepositoryB untuk Operasi Barang

---
+++ app/src/main/java/com/example/ucp2/data/repository/RepositoryB.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2.data.repository
+
+import com.example.ucp2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryB {
+    suspend fun insertBarang(barang: Barang)
+    fun getAllBarang(): Flow<List<Barang>>
+    fun getBarang(id_B: String): Flow<Barang>
+    suspend fun deleteBarang(barang: Barang)
+    suspend fun updateBarang(barang: Barang)
+}
