a48ab31838c9877debeaf3cd96ce5d3e182f93f7
safirasn21 <safirasn021@gmai.com>
2024-12-18 21:48:49 +0700

    menambahkan code confirm hapus data barang

---
+++ app/src/main/java/com/example/ucp2/ui/view/barang/DetailBrgView.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.ui.view.barang
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {/*do nothing*/},
+        title = {
+            Text("Delete Data") },
+        text = {
+            Text("Apakah anda yakin ingin menghapus data ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
