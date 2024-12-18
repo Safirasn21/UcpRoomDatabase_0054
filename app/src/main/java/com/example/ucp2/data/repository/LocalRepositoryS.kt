60ebace683e6ee88235d4b2044bc9312716a13ab
safirasn21 <safirasn021@gmai.com>
2024-12-18 21:02:22 +0700

    menambahkan oprasi untuk create/insert data suplier

---
+++ app/src/main/java/com/example/ucp2/data/repository/LocalRepositoryS.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2.data.repository
+
+import com.example.ucp2.data.dao.SuplierDao
+import com.example.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryS (
+    private val suplierDao: SuplierDao): RepositoryS {
+
+    override suspend fun insertSuplier(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    } }
