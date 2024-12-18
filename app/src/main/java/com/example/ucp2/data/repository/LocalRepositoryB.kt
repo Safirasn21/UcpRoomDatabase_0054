9bd0434ea27526d860e8717fab4515863c13cb46
safirasn21 <safirasn021@gmai.com>
2024-12-18 20:58:47 +0700

    menambahkan oprasi insert/create barang

---
+++ app/src/main/java/com/example/ucp2/data/repository/LocalRepositoryB.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2.data.repository
+
+import com.example.ucp2.data.dao.BarangDao
+import com.example.ucp2.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryB (
+    private val barangDao: BarangDao): RepositoryB {
+    override suspend fun insertBarang(barang: Barang) {
+        barangDao.insertBarang(barang)
+    }
+
+}
