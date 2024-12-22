d7dc7323c02ad7ea5ad0c22e4d3364f0e5baeb95
safirasn21 <safirasn021@gmai.com>
2024-12-22 18:54:14 +0700

    menambahkan data class formerror dengan validasi

--- app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BrgViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BrgViewModel.kt
@@ -2,6 +2,19 @@ package com.example.ucp2.ui.viewmodel.barang
 
 import com.example.ucp2.data.entity.Barang
 
+data class FormErrorState(
+    val id_B: String? = null,
+    val name: String? = null,
+    val desc: String? = null,
+    val price: String? = null,
+    val stock: String? = null,
+    val supname: String? = null
+){
+    fun isValid(): Boolean{
+        return id_B == null && name == null && desc == null && price == null
+                && stock == null && supname == null
+    }
+}
 
 fun BarangEvent.toBarangEntity(): Barang = Barang(
     id_B = id_B,
