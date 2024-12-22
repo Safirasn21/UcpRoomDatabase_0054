453d1c220ad60c71fd13ce11114a3ffaff9fedc9
safirasn21 <safirasn021@gmai.com>
2024-12-22 22:46:33 +0700

    menambahkan card Barang dengan Informasi yang Lengkap

--- app/src/main/java/com/example/ucp2/ui/view/barang/HomeBrgView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/barang/HomeBrgView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
@@ -18,11 +19,16 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -30,88 +36,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.ui.customwidget.TopAppBar
+import com.example.ucp2.ui.viewmodel.barang.BrgViewModel
+import com.example.ucp2.ui.viewmodel.barang.HomeBrgViewModel
 import com.example.ucp2.ui.viewmodel.barang.HomeUiState
+import com.example.ucp2.ui.viewmodel.barang.PenyediaBrgViewModel
 import kotlinx.coroutines.launch
 
-@Composable
-fun BodyHomeBrgView(
-    homeUiState: HomeUiState,
-    onClick: (String) -> Unit = { },
-    modifier: Modifier = Modifier
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val snackbarHostState = remember { SnackbarHostState() } // Snackbar State
-    when {
-        homeUiState.isLoading -> {
-            // Menampilkan indikator loading
-            Box(
-                modifier = modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator()
-            }
-        }
 
-        homeUiState.isError -> {
-            // Menampilkan pesan error
-            LaunchedEffect (homeUiState.errorMessage) {
-                homeUiState.errorMessage?.let { message ->
-                    coroutineScope.launch {
-                        snackbarHostState.showSnackbar(message) // Tampilkan Snackbar
-                    }
-                }
-            }
-        }
 
-        homeUiState.listBrg.isEmpty() -> {
-            // Menampilkan pesan jika data kosong
-            Box(
-                modifier = modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "Tidak ada data barang.",
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-        }
-
-        else -> {
-            ListBarang(
-                listBrg = homeUiState.listBrg,
-                onClick = {
-                    onClick(it)
-                    println(it)
-                },
-                modifier = modifier
-            )
-        }
-    }
-}
-
-@Composable
-fun ListBarang(
-    listBrg: List<Barang>,
-    modifier: Modifier = Modifier,
-    onClick: (String) -> Unit = { }
-) {
-    LazyColumn (
-        modifier = modifier
-    ) {
-        items(
-            items = listBrg,
-            itemContent = { brg ->
-                CardBrg(
-                    brg = brg,
-                    onClick = { onClick(brg.id_B) }
-                )
-            }
-        )
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
