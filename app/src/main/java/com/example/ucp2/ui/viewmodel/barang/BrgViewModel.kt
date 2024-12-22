3bfd2469629073792076895b53f1d6d3c693870b
safirasn21 <safirasn021@gmai.com>
2024-12-22 18:41:33 +0700

    menambahkan data class Barang

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BrgViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.ui.viewmodel.barang
+
+data class BarangEvent(
+    val id_B: String =" ",
+    val name: String = "",
+    val desc: String = "",
+    val price: String = "",
+    val stock: String = "",
+    val supname: String =""
+)
