c14eaa63a4032cef106595c855aa6caa59cb9b72
safirasn21 <safirasn021@gmai.com>
2024-12-22 18:45:31 +0700

    menambahkan extention fungsi barangevent ke barangentity

--- app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BrgViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BrgViewModel.kt
@@ -1,5 +1,17 @@
 package com.example.ucp2.ui.viewmodel.barang
 
+import com.example.ucp2.data.entity.Barang
+
+
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    id_B = id_B,
+    name = name,
+    desc = desc,
+    price = price,
+    stock = stock,
+    supname = supname
+)
+
 data class BarangEvent(
     val id_B: String =" ",
     val name: String = "",
