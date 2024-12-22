56960e0dec06e046e380adad3570b288e6455c99
safirasn21 <safirasn021@gmai.com>
2024-12-22 22:39:28 +0700

    menambahkan fungsi ekstensi barang toDetailEvent

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/DetailBrgViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.ui.viewmodel.barang
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.data.repository.RepositoryB
+import com.example.ucp2.ui.navigasi.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+
+fun Barang.toDetailUiEvent(): BarangEvent{
+    return BarangEvent(
+        id_B = id_B,
+        name = name,
+        desc = desc,
+        price = price,
+        stock = stock,
+        supname = supname
+    )
+}
