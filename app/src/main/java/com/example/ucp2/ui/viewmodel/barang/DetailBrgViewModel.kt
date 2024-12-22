a92c1477597c7255acca68c19409f21198a18a9d
safirasn21 <safirasn021@gmai.com>
2024-12-22 22:42:16 +0700

    menambahkan DetailBrgViewModel dengan StateFlow dan Fungsi deleteBrg

--- app/src/main/java/com/example/ucp2/ui/viewmodel/barang/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/DetailBrgViewModel.kt
@@ -16,7 +16,48 @@ import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
+class DetailBrgViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryB: RepositoryB,
+) : ViewModel(){
+    private val _id_B: String = checkNotNull(savedStateHandle[DestinasiDetail.ID_B])
 
+    val detailUIState: StateFlow<DetailUiState> = repositoryB.getBarang(_id_B)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+    fun deleteBrg(){
+        detailUIState.value.detailUiEvent.toBarangEntity().let{
+            viewModelScope.launch {
+                repositoryB.deleteBarang(it)
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent: BarangEvent = BarangEvent(),
