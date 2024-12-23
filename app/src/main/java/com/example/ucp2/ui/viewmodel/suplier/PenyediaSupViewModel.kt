3941bbf319bbc98ecba6c57345f20d35803ec7cd
safirasn21 <safirasn021@gmai.com>
2024-12-23 23:35:50 +0700

    Menambahkan PenyediaSupViewModel untuk menyediakan instance SupViewModel dan HomeSupViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/suplier/PenyediaSupViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/suplier/PenyediaSupViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucp2.TokoApp
-import com.example.ucp2.data.repository.LocalRepositoryS
+import com.example.ucp2.repository.LocalRepositoryS
 
 
 object PenyediaSupViewModel{
