0b31d2cb078e322107a431b1bb1d01aa6d84848b
safirasn21 <safirasn021@gmai.com>
2024-12-23 22:07:20 +0700

    merubah code object suplier list sebelumnya yang terdapat kesalahan

--- app/src/main/java/com/example/ucp2/data/SuplierList.kt
+++ app/src/main/java/com/example/ucp2/data/SuplierList.kt
@@ -1,17 +1,34 @@
 package com.example.ucp2.data
 
-data class Supplier(val id: String, val name: String)
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.viewmodel.suplier.HomeSupViewModel
+import com.example.ucp2.ui.viewmodel.suplier.PenyediaSupViewModel
+
+//data class Supplier(val id: String, val name: String)
+//
+//object SuplierList {
+//
+//    val suppliers = listOf(
+//        Supplier("1", "Supplier Ulfa"),
+//        Supplier("2", "Supplier Iffa"),
+//        Supplier("3", "Supplier Namira"),
+//        Supplier("4", "Supplier Rara")
+//    )
+//
+//    fun getAllSuppliers(): List<Supplier> {
+//        return suppliers
+//    }
+//}
 
 object SuplierList {
-
-    val suppliers = listOf(
-        Supplier("1", "Supplier Ulfa"),
-        Supplier("2", "Supplier Iffa"),
-        Supplier("3", "Supplier Namira"),
-        Supplier("4", "Supplier Rara")
-    )
-
-    fun getAllSuppliers(): List<Supplier> {
-        return suppliers
+    @Composable
+    fun Supname(
+        SupViewModel : HomeSupViewModel = viewModel(factory = PenyediaSupViewModel.Factory)
+    ): List<String>{
+        val suplname = SupViewModel.homeUiStates.collectAsState()
+        val suplist = suplname.value.listSup
+        return suplist.map {it.supname}
     }
 }
