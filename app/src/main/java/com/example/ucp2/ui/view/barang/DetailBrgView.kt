4f9dfa0363b029b5f9e0bdc9ca2fb346c699d084
safirasn21 <safirasn021@gmai.com>
2024-12-23 18:09:58 +0700

    memperbaiki code yang error

--- app/src/main/java/com/example/ucp2/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/barang/DetailBrgView.kt
@@ -1,16 +1,183 @@
 package com.example.ucp2.ui.view.barang
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.ui.viewmodel.barang.DetailBrgViewModel
+import com.example.ucp2.ui.viewmodel.barang.DetailUiState
+import com.example.ucp2.ui.viewmodel.barang.PenyediaBrgViewModel
+import com.example.ucp2.ui.viewmodel.barang.toBarangEntity
+
+@Preview
+@Composable
+fun DetailBrgView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailBrgViewModel = viewModel(factory = PenyediaBrgViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar barang",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUIState.value.detailUiEvent.id_B) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Barang",
+                )
+            }
+        }
+    ){ innerPadding ->
+        val detailUiState by viewModel.detailUIState.collectAsState()
+
+        BodyDetailBrg(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteBrg()
+                onDeleteClick()
+            }
+        )
+
+    }
+}
+@Composable
+fun BodyDetailBrg(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = { }
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when{
+        detailUiState.isLoading -> {
+            Box (
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ){
+                ItemDetailBrg(
+                    barang = detailUiState.detailUiEvent.toBarangEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationRequired){
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box (
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+@Composable
+fun ItemDetailBrg(
+    modifier: Modifier = Modifier,
+    barang: Barang
+){
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    ){
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailBrg(judul = "ID", isinya = barang.id_B)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Nama", isinya = barang.name)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Desc", isinya = barang.desc)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Price", isinya = barang.price)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Stock", isinya = barang.stock)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Supname", isinya = barang.supname)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailBrg(
