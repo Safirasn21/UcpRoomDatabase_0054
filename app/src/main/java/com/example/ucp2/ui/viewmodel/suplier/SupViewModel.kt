ef2f0e0e9ec9af7008eafc21dbc6db21acc8fb21
safirasn21 <safirasn021@gmai.com>
2024-12-23 19:41:48 +0700

    Implementasi SupViewModel untuk Mengelola Data dan Validasi Suplier

--- app/src/main/java/com/example/ucp2/ui/viewmodel/suplier/SupViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/suplier/SupViewModel.kt
@@ -9,7 +9,55 @@ import com.example.ucp2.data.repository.LocalRepositoryS
 import com.example.ucp2.ui.viewmodel.barang.FormErrorState
 import kotlinx.coroutines.launch
 
+class SupViewModel(private val repositoryS: LocalRepositoryS) : ViewModel(){
+    var uiState by mutableStateOf(SuplierUIState())
+    fun updateState(SuplierEvent: SuplierEvent){
+        uiState = uiState.copy(
+            SuplierEvent = SuplierEvent,
+        )
+    }
 
+    private fun validateFields(): Boolean{
+        val event = uiState.SuplierEvent
+        val errorState = FormErrorStates(
+            idS = if (event.id_S.isNotEmpty()) null else "Suplier Id tidak boleh kosong!",
+            supname = if (event.supname.isNotEmpty()) null else "Suplier Name tidak boleh kosong!",
+            contact = if (event.contact.isNotEmpty()) null else "Suplier contact tidak boleh kosong!",
+            address = if (event.address.isNotEmpty()) null else "Suplier address tidak boleh kosong!"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData(){
+        val currentEvent = uiState.SuplierEvent
+
+        if (validateFields()){
+            viewModelScope.launch {
+                try{
+                    repositoryS.insertSuplier(currentEvent.toSuplierEntity())
+                    uiState = uiState.copy(
+                        snackBarMesssage = "Data berhasil disimpan",
+                        SuplierEvent  = SuplierEvent(),
+                        isEntryValid  = FormErrorStates()
+                    )
+                } catch (e: Exception){
+                    uiState = uiState.copy(
+                        snackBarMesssage = "Data gagal disimpan"
+                    )
+                }
+            }
+        }
+        else {
+            uiState = uiState.copy(
+                snackBarMesssage = "Input tidak valid. periksa kembali data anda"
+            )
+        }
+    }
+    fun resetsnackBarMessage(){
+        uiState = uiState.copy(snackBarMesssage = null)
+    }
+}
 
 data class SuplierUIState(
     val SuplierEvent: SuplierEvent = SuplierEvent(),
