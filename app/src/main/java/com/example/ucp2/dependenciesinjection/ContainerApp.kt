2dfdaaf75ee860303aa22b0766901118e669298a
safirasn21 <safirasn021@gmai.com>
2024-12-23 23:34:20 +0700

    Menambahkan implementasi InterfaceContainerApp untuk inisialisasi repository

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,18 +1,18 @@
-package com.example.ucp2.data.dependenciesinjection
+package com.example.ucp2.dependenciesinjection
 
 import android.content.Context
 import com.example.ucp2.data.database.TokoDatabase
-import com.example.ucp2.data.repository.LocalRepositoryB
-import com.example.ucp2.data.repository.LocalRepositoryS
-import com.example.ucp2.data.repository.RepositoryB
-import com.example.ucp2.data.repository.RepositoryS
+import com.example.ucp2.repository.LocalRepositoryB
+import com.example.ucp2.repository.LocalRepositoryS
+import com.example.ucp2.repository.RepositoryB
+import com.example.ucp2.repository.RepositoryS
 
 interface InterfaceContainerApp {
     val repositoryB: RepositoryB
     val repositoryS: RepositoryS
 }
 
-class ContainerApp(private val context: Context): InterfaceContainerApp{
+class ContainerApp(private val context: Context): InterfaceContainerApp {
     override val repositoryB: RepositoryB by lazy {
         LocalRepositoryB(TokoDatabase.getDatabase(context).barangDao())
     }
