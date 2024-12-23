77e0d4f0da4bca73ed754b2631feca40f5be5587
safirasn21 <safirasn021@gmai.com>
2024-12-24 02:35:49 +0700

    Menambahkan Header untuk halaman HomeView

--- app/src/main/java/com/example/ucp2/ui/view/HomeView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/HomeView.kt
@@ -1,14 +1,47 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.ucp2.R
 
+@Composable
+fun SectionHeader() {
+    Box(
+        modifier = Modifier
+        .fillMaxWidth()
+        .background(color = Color.Blue))
+    Row {
+        Box(contentAlignment = Alignment.BottomEnd){
+            Image(painter = painterResource(id = R.drawable.img), contentDescription = " ",
+                Modifier
+                    .size(80.dp)
+                    .clip(shape = CircleShape)
+            )
+        }
+        Column (Modifier.padding(15.dp)){
+            Text(text = "Halo!", color = Color.White)
+            Text(text = "Safira", color = Color.White)
+        }
+    }
+}
 @Composable
 fun HomeView(
     modifier: Modifier = Modifier,
