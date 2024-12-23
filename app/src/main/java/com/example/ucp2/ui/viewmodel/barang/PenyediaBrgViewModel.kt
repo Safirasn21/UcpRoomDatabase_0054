d2c9c6179038c80554bf06ab33fce2f92f7dda3a
safirasn21 <safirasn021@gmai.com>
2024-12-23 17:33:02 +0700

    Inisialisasi Factory untuk ViewModel Barang

--- app/src/main/java/com/example/ucp2/ui/viewmodel/barang/PenyediaBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/PenyediaBrgViewModel.kt
@@ -7,6 +7,32 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucp2.TokoApp
 
+object PenyediaBrgViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            BrgViewModel(
+                TokoApp().containerApp.repositoryB
 
-fun CreationExtras.krsApp(): TokoApp =
+            )
+        }
+        initializer {
+            HomeBrgViewModel(
+                TokoApp().containerApp.repositoryB
+            )
+        }
+        initializer {
+            DetailBrgViewModel(
+                createSavedStateHandle(),
+                TokoApp().containerApp.repositoryB,
+            )
+        }
+        initializer {
+            UpdateBrgViewModel(
+                createSavedStateHandle(),
+                TokoApp().containerApp.repositoryB,
+            )
+        }
+    }
+}
+fun CreationExtras.TokoApp(): TokoApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TokoApp)
